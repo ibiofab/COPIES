bf0d7f12a98fae770a3bec0fb8c32296ecf9f94d
Aashutosh Boob <60017121+aashutoshb97@users.noreply.github.com>
2023-03-30 16:42:25 -0500

    Avoiding deletion in ID column

--- code/main.py
+++ code/main.py
@@ -1070,9 +1070,6 @@ def main():
         grna_hr_df.insert(loc = 3, column='Self-Complementarity', value = self_comp)
         grna_hr_df.insert(loc = 3, column='GC Content', value=[GC(w) for w in list(grna_hr_df['Guide Sequence'])])
 
-        #Adding Sequence ID to track gRNA in visualization (as Bokeh does not allow copying)
-        grna_hr_df.insert(0, 'ID', range(1, 1 + len(grna_hr_df)))
-
         #Remove gRNA located at the end of the chromosomes
         ind_to_remove = []
         for i in range(len(grna_hr_df)):
@@ -1080,6 +1077,9 @@ def main():
                 ind_to_remove.append(i)
 
         grna_hr_df = grna_hr_df.drop(ind_to_remove).reset_index(drop=True)
+
+        #Adding Sequence ID to track gRNA in visualization (as Bokeh does not allow copying)
+        grna_hr_df.insert(0, 'ID', range(1, 1 + len(grna_hr_df)))
 
         #On target scores
         if len(grna_hr_df) > 0:
